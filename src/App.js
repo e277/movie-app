14d4d8599821de8e11c9e8927d35c3fa60f8b190
Ezra Muir <ezramuir12@gmail.com>
2022-05-05 14:36:59 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 2aec1b91 - api key
+const API_URL = `http://www.omdbapi.com/?i=tt3896198&apikey=2aec1b91`
+
+const App = () => {
+
+    const serchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json()
+        console.log(data.Search)
+    }
+
+    useEffect(() => {
+        serchMovies('Spiderman')
+    }, [])
+
+
+    return (
+        <div className="App">
+            App
+        </div>
+    );
 }
 
 export default App;
